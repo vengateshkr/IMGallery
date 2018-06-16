bb7e2f1273573262f9a56afb789f508dce638453
Venkatesh <>
2018-06-16 14:06:10 +0530

    Initial commit

--- IMGallery/ViewController.swift
+++ IMGallery/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .white
         // Do any additional setup after loading the view, typically from a nib.
     }
 
