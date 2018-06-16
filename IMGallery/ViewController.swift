c42e7fb8252e2592a2ccf3421bff3d2de0394ae1
Venkatesh <>
2018-06-16 14:04:31 +0530

    Initial commit

---
+++ IMGallery/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IMGallery
+//
+//  Created by Venkatesh on 16/06/18.
+//  Copyright © 2018 Venkatesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
